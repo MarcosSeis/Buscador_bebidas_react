7bbe002d8bb861c353e0607b164fe3666ad1bf8b
Marcos <marsanmar6291@gmail.com>
2023-03-01 13:56:37 -0600

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { Button, Form, Row, Col, Alert } from "react-bootstrap";
+import useCategorias from "../hooks/useCategorias";
+import useBebidas from "../hooks/useBebidas";
+
+function Formulario() {
+
+    const [busqueda, setBusqueda] = useState({
+        nombre: '',
+        categoria: ''
+    })
+
+    const [alerta, setAlerta] = useState('')
+
+    const { categorias } = useCategorias();
+    const { consultarBebidas } = useBebidas();
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        if(Object.values(busqueda).includes('')){
+            setAlerta('Todos los campos son obligatorios');
+            return
+        }
+
+        setAlerta('');
+        consultarBebidas(busqueda);
+
+
+
+    }
+
+  return (
+    <Form
+        onSubmit={handleSubmit}
+        >
+            {alerta && <Alert variant="danger" className="text-center">{alerta}</Alert>}
+
+        <Row>
+            <Col md={6}>
+                <Form.Group>
+                    <Form.Label htmlFor="nombre"> Buscador Bebida</Form.Label>
+                    <Form.Control 
+                        id='nombre'
+                        type='text'
+                        placeholder='Ej: Tequila, Vodka'
+                        name='nombre'
+                        value={busqueda.nombre}
+                        onChange={e => setBusqueda({
+                            ...busqueda,
+                            [e.target.name] : e.target.value
+                        })}
+                        />
+                </Form.Group>
+            </Col>
+            <Col md={6}>
+            <Form.Group>
+                    <Form.Label htmlFor="categoria"> Categoria Bebida</Form.Label>
+                    <Form.Select
+                        id='categoria'
+                        name='categoria'
+                        value={busqueda.categoria}
+                        onChange={e => setBusqueda({
+                            ...busqueda,
+                            [e.target.name] : e.target.value
+                        })}
+                        >
+                        <option value="">--Selecciona Categoria --</option>
+                        
+                        {categorias.map(categoria => (
+                            <option 
+                                key={categoria.strCategory}
+                                value={categoria.strCategory}>
+                                {categoria.strCategory}
+                                </option>
+                        ))}
+
+                    </Form.Select>
+                </Form.Group>
+            </Col>
+        </Row>
+        
+        <Row className="justify-content-end mt-3">
+            <Col md={3}>
+                <Button
+                    variant="danger"
+                    className="text-upercase w-100"
+                    type="submit"
+                    >
+                    Buscar Bebidas
+                </Button>
+            </Col>
+        </Row>
+
+    </Form>
+  )
+}
+
+export default Formulario
---
+++ src/components/ListadoBebidas.jsx
@@ -0,0 +1,22 @@
+import Bebida from "./Bebida";
+import useBebidas from "../hooks/useBebidas";
+import { Row } from "react-bootstrap";
+
+function ListadoBebidas() {
+
+    const { bebidas } = useBebidas();
+
+
+  return (
+    <Row className="mt-5">
+        {bebidas.map(bebida => 
+            <Bebida 
+                key={bebida.idDrink}
+                bebida={bebida}
+                />
+            )}
+    </Row>
+  )
+}
+
+export default ListadoBebidas
